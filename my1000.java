8cece5f92e08aabfa3901ce4becd3c88a837b80a
liangrujian <liangrujian@sina.com>
2015-11-07 06:30:33 +0800

    1000

---
+++ my1000.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a + b);
+        in.close();
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+	}
+
+}
